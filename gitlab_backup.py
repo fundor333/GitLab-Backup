d83a563b17337e1e8aee5612c78cffee561095d3
Fundor333 <github@fundor333.com>
2019-01-18 22:16:42 +0100

    Reworking some function

--- gitlab_backup.py
+++ gitlab_backup.py
@@ -6,52 +6,69 @@ import io
 import urllib, json
 import urllib.parse
 import urllib.request
+from datetime import datetime
 
 
-try:
-    with open("settings.json", "r") as stream:
-        settings = json.load(stream)
-except FileNotFoundError:
-    settings = {
-        "GitLab": {
-            "Base url": "https://gitlab.com",
-            "Personal token": "<PUT HERE THE TOKEN>",
-        },
-        "Repositories": {"Path": "repos/", "Archived": []},
-    }
-    with io.open("settings.json", "w", encoding="utf8") as outfile:
-        json.dump(settings, outfile)
-    exit()
-
-for repo in settings["Repositories"]["Archived"]:
-    bash_command = "git -C {} pull --all".format(
-        settings["Repositories"]["Path"] + repo["path"]
-    )
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+def get_settings():
+    try:
+        with open("settings.json", "r") as stream:
+            settings = json.load(stream)
+        return settings
+    except FileNotFoundError:
+        settings = {
+            "GitLab": {
+                "Base url": "https://gitlab.com",
+                "Personal token": "<PUT HERE THE TOKEN>",
+            },
+            "Repositories": {"Path": "repos/", "Archived": []},
+        }
+        with io.open("settings.json", "w", encoding="utf8") as outfile:
+            json.dump(settings, outfile)
+        exit()
 
 
-print("Starting the download")
+def manage_repo(list_archived, settings, repo):
+    if repo["path_with_namespace"] in list_archived.keys():
+        print("{} is old repo".format(repo["path_with_namespace"]))
+        bash_command = "git -C {} pull --all".format(
+            settings["Repositories"]["Path"] + repo["path"]
+        )
 
-url = settings["GitLab"]["Base url"] + "/api/v4/projects"
-headers = {"Private-Token": settings["GitLab"]["Personal token"]}
-
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    r = json.loads(response.read().decode())
-
-    for repo in r:
+    else:
+        print("{} is new repo".format(repo["path_with_namespace"]))
         bash_command = "git clone --recurse-submodules {} {}".format(
             repo["http_url_to_repo"],
             settings["Repositories"]["Path"] + repo["path_with_namespace"],
         )
-        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
 
         settings["Repositories"]["Archived"].append(
             {"url": repo["http_url_to_repo"], "path": repo["path_with_namespace"]}
         )
 
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
     with io.open("settings.json", "w") as outfile:
         json.dump(settings, outfile)
-    exit()
+
+
+if __name__ == "__main__":
+    settings = get_settings()
+
+    print("Starting the download")
+
+    url = settings["GitLab"]["Base url"] + "/api/v4/projects"
+    headers = {"Private-Token": settings["GitLab"]["Personal token"]}
+
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        r = json.loads(response.read().decode())
+        list_archived = {}
+        for e in settings["Repositories"]["Archived"]:
+            list_archived[e["path"]] = e["url"]
+        for repo in r:
+            manage_repo(list_archived, settings, repo)
+
+        settings["Last"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        with io.open("settings.json", "w") as outfile:
+            json.dump(settings, outfile)
+        exit()
